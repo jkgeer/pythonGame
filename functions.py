b2aa62714a08b28b44bff56e7a4b185ea337e200
odorsey <odorsey@live.unc.edu>
2013-12-08 14:08:27 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,7 +7,9 @@ def player_input (prompt, choices_list):
 choices_list = ['Female', 'Male'];
 for choices_list[0] in choices_list:
     import barmaid_level1
-
+for choices_list[1] in choices_list:
+    import scribe_level1
+    
 while input not in choices_list:
     input = raw_input("What is your choice? \n")
     print input
