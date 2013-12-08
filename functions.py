681449c5f848dd17c6ac21f88c30e0c64ede35b1
odorsey <odorsey@live.unc.edu>
2013-12-08 13:10:05 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 # INTRODUCTION LEVEL 
-dict player_input {prompt, choices_list};
+var player_input (prompt, choices_list);
 player[choice] = player_input("Will you be a female or male in this game?", ["Female", "Male"];)
 choices_list = list ["Female", "Male"]; 
 input =""
